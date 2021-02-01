5d96588d4bc248846b87e4ca0b06c7e87ede4880
Kerven <Kervenlaurenttsic@gmail.com>
2021-01-31 19:23:29 -0500

    initialization

--- codeproject/code.js
+++ codeproject/code.js
@@ -1,7 +1,9 @@
-var firstName; var lastName; var age;
+var firstName; 
+var lastName;
+var age;
 firstName = Kerven;
 lastName = Laurent;
 age = 21;
-alert(My name is firstName lastName)
-alert(I am age years old)
-console.log(I am having a lot of fun taking this class. Even though it may get confusing at times it is still enjoyable figuring it out.)
+alert('My name is firstName lastName')
+alert('I am age years old')
+console.log("I am having a lot of fun taking this class. Even though it may get confusing at times it is still enjoyable figuring it out.")

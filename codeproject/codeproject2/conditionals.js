a64364dc09e3e7fdb3e7c7d0a77df42396ce2f92
Kerven <Kervenlaurenttsic@gmail.com>
2021-02-07 13:42:11 -0500

    initialization

---
+++ codeproject/codeproject2/conditionals.js
@@ -0,0 +1,20 @@
+var userChoice; var randomNumber; var computerChoice;
+userChoice = prompt("Heads or Tails?");
+let randomNumber = (Math.random()>=0.5)? 1 : 0;
+fucntion ;test (randomNumber) ;{
+    if (randomNumber === 0) {
+        return "Heads";
+    }
+    if (randomNumber === 1) {
+        return "Tails";
+    }
+}
+computerChoice = Heads;
+computerChoice = Tails;
+function equalityTest (userChoice) {
+    if (userChoice == computerChoice) {
+        return alert ('You guessed right! The coin flip landed on [YOUR CHOICE HERE]');
+    }
+    else
+    return alert ('Sorry, the coin flip landed on [NOT YOUR CHOICE HERE]');
+}
